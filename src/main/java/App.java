ebc24e98ded4245403592ccdf193616074ae8fb5
VitalyLemeshevsky <112518266+VitalyLemeshevsky@users.noreply.github.com>
2024-04-10 15:16:54 +0300

    test commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,5 @@
-package by.mdased;
-
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!!");
     }
 }
